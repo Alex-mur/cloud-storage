fbcef7743d4197f5c2b0c5cb26bd75980e18b2bc
Alex <alex-mur@outlook.com>
2018-07-15 23:26:44 +0300

    add logout button. add progressBar. some other improovements in UI. Created connectionHandler class.

--- client/src/main/java/com/cloud/storage/client/Controller.java
+++ client/src/main/java/com/cloud/storage/client/Controller.java
@@ -3,13 +3,20 @@ package com.cloud.storage.client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import javax.swing.text.html.ListView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    Button btnLogout;
+
+    @FXML
+    HBox chooseLocalDirArea;
 
     @FXML
     VBox mainArea;
@@ -26,6 +33,16 @@ public class Controller implements Initializable {
     @FXML
     VBox localListArea;
 
+    @FXML
+    ProgressBar pBar;
+
+    @FXML
+    VBox logArea;
+
+
+    private boolean isConnected;
+    private boolean isLocalDirChoosed;
+
 
     public void initialize(URL url, ResourceBundle rb) {
         localListArea.setManaged(false);
@@ -34,7 +51,9 @@ public class Controller implements Initializable {
         remoteListArea.setVisible(false);
         transferBtnArea.setVisible(false);
         transferBtnArea.setManaged(false);
-
+        isConnected = false;
+        isLocalDirChoosed =false;
+        pBar.prefWidthProperty().bind(logArea.widthProperty());
     }
 
     public void btnConnectClick() {
@@ -42,13 +61,35 @@ public class Controller implements Initializable {
         loginArea.setVisible(false);
         remoteListArea.setManaged(true);
         remoteListArea.setVisible(true);
-        transferBtnArea.setVisible(true);
-        transferBtnArea.setManaged(true);
+        isConnected = true;
+        if (isLocalDirChoosed) {
+            transferBtnArea.setVisible(true);
+            transferBtnArea.setManaged(true);
+        }
     }
 
     public void btnChangeDirClick(ActionEvent actionEvent) {
     }
 
     public void btnChooseDirClick(ActionEvent actionEvent) {
+        isLocalDirChoosed = true;
+        chooseLocalDirArea.setManaged(false);
+        chooseLocalDirArea.setVisible(false);
+        localListArea.setManaged(true);
+        localListArea.setVisible(true);
+        if (isConnected) {
+            transferBtnArea.setVisible(true);
+            transferBtnArea.setManaged(true);
+        }
+    }
+
+    public void btnLogoutClick(ActionEvent actionEvent) {
+        isConnected = false;
+        remoteListArea.setManaged(false);
+        remoteListArea.setVisible(false);
+        transferBtnArea.setVisible(false);
+        transferBtnArea.setManaged(false);
+        loginArea.setManaged(true);
+        loginArea.setVisible(true);
     }
 }
